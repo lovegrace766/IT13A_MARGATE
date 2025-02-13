db1ee79d8b699eaccd1dbb7cf5c5db98fac16066
lovegrace766 <margatelovegrace09@gmail.com>
2025-02-13 18:39:31 +0800

    added HELLO WORLD

---
+++ IT13A_MARGATE/src/main/java/PRELIM/LAB_ACT1_ARITHMETICS.java
@@ -0,0 +1,27 @@
+/*
+ * 
+ */
+package PRELIM;
+
+/**
+ *
+ * @author Administrator
+ */
+public class LAB_ACT1_ARITHMETICS {
+
+ public static void main(String[] args) {
+ int firstnumber= 10;
+ int secondnumber= 4;
+ int thirdnumber= 6;
+ 
+int addition = firstnumber + secondnumber + thirdnumber;
+int subtraction = firstnumber - secondnumber - thirdnumber;
+int multiply = firstnumber * secondnumber * thirdnumber;
+int divide = firstnumber / secondnumber / thirdnumber;
+int module = firstnumber % secondnumber % thirdnumber;
+
+System.out.println("10 + 4 + 6 = " + addition);
+System.out.println("10 - 4 - 6 = " + subtraction);
+System.out.println("10 * 4 * 6 = " + multiply);
+System.out.println("10 / 4 / 6 = " + divide);
+System.out.println("10 % 4 % 6 = " + module); 
