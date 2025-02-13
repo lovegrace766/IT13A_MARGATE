262bae75d96f45d21f9260ab375be58dfa1d730a
lovegrace766 <margatelovegrace09@gmail.com>
2025-02-13 17:44:43 +0800

    ADDED JAVA APPLICATION

---
+++ IT13A_MARGATE/src/main/java/com/mycompany/it13a_margate/IT13A_MARGATE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.it13a_margate;
+
+/**
+ *
+ * @author Administrator
+ */
+public class IT13A_MARGATE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
